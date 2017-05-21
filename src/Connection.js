bf0d6a5b25bb96f8d3bcdd7e7201d91e8935ee6e
Jett Carlo Calleja <jettcalleja@gmail.com>
2017-05-21 15:29:36 +0800

    Fixed bug on creating another pool connection when chaging keys and using transaction (#11)
    * Fixed bug on creating another pool connection when chaging keys and using transaction
    
    * Updated syntax

--- src/Connection.js
+++ src/Connection.js
@@ -18,7 +18,7 @@ export default class Connection {
 
         if (handle[key].is_pool) {
             // handle._logger.log('Added a pool connection for', key);
-            connection = mysql.createPool(handle[key].config);
+            connection = handle[key].connection || mysql.createPool(handle[key].config);
         }
         else {
             // handle._logger.log('Created a single connection');
